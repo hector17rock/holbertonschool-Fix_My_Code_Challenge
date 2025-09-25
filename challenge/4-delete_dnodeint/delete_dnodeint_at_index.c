44fe5eea43e99ae2fa3bf42fe35c5db5adba84ec
Hector Soto <hector_17rock@hotmail.com>
2025-09-24 20:04:10 -0400

    TASK04

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,44 +2,52 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
- * @head: pointer to pointer to head of list
- * @index: zero-based index of node to delete
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *node;
-	unsigned int i;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
+	{
 		return (-1);
-
-	node = *head;
-
-	/* Walk to the node at `index` */
-	for (i = 0; node != NULL && i < index; i++)
-		node = node->next;
-
-	/* If index is out of range */
-	if (node == NULL)
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
 		return (-1);
-
-	/* Stitch neighbors together correctly */
-	if (node->prev != NULL)
-		node->prev->next = node->next;
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
 	else
-		*head = node->next;           /* deleting the head */
-
-	if (node->next != NULL)
-		node->next->prev = node->prev;
-
-	free(node);
-
-	/* Ensure new head (if any) has prev == NULL */
-	if (*head != NULL)
-		(*head)->prev = NULL;
-
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
 	return (1);
 }
